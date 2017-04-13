5291ad9aed2e2af69f2c23c011cc9eae965a1e7b
Andrea Zucchini <azga84@gmail.com>
2017-04-14 01:15:46 +0200

    fetch function

--- lib/github_issues_client/http.ex
+++ lib/github_issues_client/http.ex
@@ -1,4 +1,20 @@
-defmodule Http do
-  @moduledoc false
-  
+defmodule GithubIssuesClient.Http do
+  @user_agent [ {"User-agent", "zucchinidev"}]
+  def fetch(user, project) do
+    issues_url(user, project)
+      |> HTTPoison.get(@user_agent)
+      |> handle_response
+  end
+
+  defp issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  defp handle_response({ :ok, %{status_code: 200, body: body }}) do
+      { :ok, body }
+  end
+
+  defp handle_response({ _ , %{status_code: _, body: body }}) do
+        { :error, body }
+    end
 end
