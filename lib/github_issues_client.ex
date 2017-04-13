ba6c3f3a1d626801d3d1c37e4482f9da54897208
Andrea Zucchini <azga84@gmail.com>
2017-04-13 23:39:25 +0200

    Initial commit of github client

---
+++ lib/github_issues_client.ex
@@ -0,0 +1,18 @@
+defmodule GithubIssuesClient do
+  @moduledoc """
+  Documentation for GithubIssuesClient.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GithubIssuesClient.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/github_issues_client_test.exs
@@ -0,0 +1,8 @@
+defmodule GithubIssuesClientTest do
+  use ExUnit.Case
+  doctest GithubIssuesClient
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
