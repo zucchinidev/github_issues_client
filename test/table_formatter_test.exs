b491e5193d783dfa391152f4af28eb6c8a3823a0
Andrea Zucchini <azga84@gmail.com>
2017-04-16 02:21:16 +0200

    capture result sent to stdout

--- test/table_formatter_test.exs
+++ test/table_formatter_test.exs
@@ -7,8 +7,11 @@ defmodule TableFormatterTest do
                                            get_fields: 1,
                                            create_row: 2,
                                            get_separator: 0,
-                                           get_header: 0
+                                           get_header: 0,
+                                           show_result: 1
                                          ]
+  import ExUnit.CaptureIO
+
   test "padding word with a character" do
     left = pad("foo")
     result = padding_word("foo", "*")
@@ -43,6 +46,15 @@ defmodule TableFormatterTest do
     assert header == get_header()
   end
 
+  test "correct format returned" do
+    result = capture_io(fn ->
+      show_result(get_mock_data())
+    end)
+    assert result == """
+    #{simple_test_data()}
+    """
+  end
+
   defp pad(word) do
     String.pad_trailing(word, 50, "*")
   end
@@ -64,8 +76,8 @@ defmodule TableFormatterTest do
        },
       %{
         "created_at" => "2016-03-20T14:57:25Z",
-        "id" => 142171742, "labels" => [],
-        "title" => "Asset paths are wrong when forwarding in router",
+        "id" => 142171742,
+        "title" => "Asset paths are wrong when forwarding in router"
       }
     ]
   end
@@ -73,10 +85,26 @@ defmodule TableFormatterTest do
   defp get_mock_separator do
     line = "--------------------------------------------------"
     plus = " + "
-    result = [line, plus, line, plus, line, plus, "\n"]
+    [line, plus, line, plus, line, plus, "\n"]
   end
 
   defp get_mock_header do
     {"ID", "Created at", "Title"} |> padding(" ") |> create_row("|")
+  end
+  
+  defp simple_test_data do
+    pad = fn (word) ->
+      String.pad_trailing(word, 50, " ")
+    end
+    separator = " | "
+    get_mock_header() ++ get_mock_separator() ++
+    [
+      pad.("125677936"), separator,
+      pad.("2016-01-08T19:34:22Z"), separator,
+      pad.("Update Phoenix website"), separator, "\n",
+      pad.("142171742"), separator,
+      pad.("2016-03-20T14:57:25Z"), separator,
+      pad.("Asset paths are wrong when forwarding in router"), separator, "\n"
+    ]
   end
 end
